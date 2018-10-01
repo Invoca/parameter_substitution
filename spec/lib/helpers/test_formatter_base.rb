00cad346caef55de0530b0b45366a40b1ca4d804
Collin McGrath <cmcgrath@invoca.com>
2018-10-01 16:34:50 -0700

    STORY-6556 create parametersubstitution gem: Add test formatter base class and use it to configure the gem during test

---
+++ spec/lib/helpers/test_formatter_base.rb
@@ -0,0 +1,11 @@
+class TestFormatterBase
+  class << self
+    def find(key)
+      # TODO: Just match on the key and return the right one
+    end
+
+    def has_parameters?
+      false
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec'
 require 'pry-byebug'
 require 'parameter_substitution'
+require_relative '../spec/lib/helpers/test_formatter_base'
+
+ParameterSubstitution.configure do |config|
+  config.method_call_base_class = TestFormatterBase
+end
