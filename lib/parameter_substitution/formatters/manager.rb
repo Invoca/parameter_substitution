d48484202fa0b4969a3ba8ab89e7bbcbb901f6e9
Collin McGrath <cmcgrath@invoca.com>
2018-11-20 14:31:31 -0800

    STORY-6650 extract generic formatters from web: Filter out manager class

--- lib/parameter_substitution/formatters/manager.rb
+++ lib/parameter_substitution/formatters/manager.rb
@@ -16,15 +16,15 @@ class ParameterSubstitution
           @default_formats ||= formatter_class_hash(__FILE__, ["ParameterSubstitution", "Formatters"])
         end
 
+        private
+
         def formatter_class_hash(manager_file, module_array)
           Hash[Dir[Pathname.new(manager_file).dirname + '*.rb'].map do |filename|
-            class_key = File.basename(filename).chomp(".rb")
+            class_key  = File.basename(filename).chomp(".rb")
             class_name = (module_array + [class_key.camelize.to_s]).join('::').constantize
             [class_key, class_name]
-          end]
+          end].except("manager")
         end
-
-        private
 
         def custom_formatters_if_any
           ParameterSubstitution.config&.custom_formatters || {}
