5fd7fff89bcff06a093b0893871d1222f90f820c
Collin McGrath <cmcgrath@invoca.com>
2018-11-27 14:28:44 -0800

    STORY-6650 extract generic formatters from web: Code was DRYer than necessary

--- lib/parameter_substitution/configuration.rb
+++ lib/parameter_substitution/configuration.rb
@@ -14,31 +14,25 @@ class ParameterSubstitution
     private
 
     def check_that_classes_are_classes(custom_formatters)
-      bad_formatters = reject_formatters_by_condition(custom_formatters) do |klass|
+      bad_formatters = custom_formatters.reject do |_formatter, klass|
         klass.is_a?(Class)
       end
 
-      raise_if_any(bad_formatters, "be of type Class")
+      raise_if_any(bad_formatters, "must be of type Class")
     end
 
     def check_for_correct_base_class(custom_formatters)
-      bad_formatters = reject_formatters_by_condition(custom_formatters) do |klass|
+      bad_formatters = custom_formatters.reject do |_formatter, klass|
         klass.ancestors.include?(ParameterSubstitution::Formatters::Base)
       end
 
-      raise_if_any(bad_formatters, "inherit from ParameterSubstitution::Formatters::Base and did not")
-    end
-
-    def reject_formatters_by_condition(custom_formatters)
-      custom_formatters.reject do |_formatter, klass|
-        yield(klass)
-      end
+      raise_if_any(bad_formatters, "must inherit from ParameterSubstitution::Formatters::Base and did not")
     end
 
     def raise_if_any(formatters, failure_context)
       if formatters.any?
         log_context = formatters.map { |formatter, klass| [formatter, klass].join(": ") }.join(", ")
-        raise StandardError, "CONFIGURATION ERROR: custom_formatters (#{log_context}) must #{failure_context}."
+        raise StandardError, "CONFIGURATION ERROR: custom_formatters (#{log_context}) #{failure_context}."
       end
     end
   end
