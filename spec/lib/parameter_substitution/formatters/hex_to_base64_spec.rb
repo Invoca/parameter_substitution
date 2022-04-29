5a2943a8b3d2ebcddaf27db942e61ecfb581c464
bhafer <bhafer@invoca.com>
2022-04-29 13:48:59 -0400

    STORY-11312 added test to check for newline characters when hex_to_base64 strings with more than 80 characters

--- spec/lib/parameter_substitution/formatters/hex_to_base64_spec.rb
+++ spec/lib/parameter_substitution/formatters/hex_to_base64_spec.rb
@@ -20,5 +20,11 @@ describe ParameterSubstitution::Formatters::HexToBase64 do
       # sha256 original value: TestValue5678
       expect(@format_class.format("fd2cf6d725e3ff42ec13be7107d4c333b691fc8cec1b9aca562c5f9848471e9d")).to eq("/Sz21yXj/0LsE75xB9TDM7aR/IzsG5rKVixfmEhHHp0=")
     end
+
+    it "converts 80+ character string to base64 without newline character" do
+      # sha256 original value: cMwwWi7qNY3HdknYjir5H2kovw1RFIlelpznZs59K3sHZCDNgvzF5hgiPI0c3LF8NJH0b3W116A9mrnk50
+      expect(@format_class.format("5329639ae1b9b6cf94e44e8021ce9c43ac1861b00bf9af3febfa0189b7d67c8b")). to eq("UyljmuG5ts+U5E6AIc6cQ6wYYbAL+a8/6/oBibfWfIs=")
+      expect('UyljmuG5ts+U5E6AIc6cQ6wYYbAL+a8/6/oBibfWfIs='.unpack("m0").first.unpack('H*').first). to eq('5329639ae1b9b6cf94e44e8021ce9c43ac1861b00bf9af3febfa0189b7d67c8b')
+    end
   end
 end
